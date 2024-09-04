48d928522db295edbac8c875181f47fe65f0dc44
Pankaja Sharma <pankajasharma2@gmail.com>
2024-09-04 09:22:47 +0530

    function committed

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const age = 24;
 const name = 'Pankaja';
 
 const dob = '12/11/2001';
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
---
+++ script.js
@@ -0,0 +1,4 @@
+function func()
+{
+    console.log("This is a function");
+}
